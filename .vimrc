a89678c46567ae9308f8dd10eccc10a1b81c9773
Robert Chrobociński <robert.chrobocinski@gmail.com>
2017-01-27 12:06:30 +0100

    Next tweaks

---
+++ .vimrc
@@ -0,0 +1,21 @@
+"REMEMBER TO ADD set exrc to your vimrc!
+
+map Q ggVG:pyf /usr/share/clang/clang-format.py<cr>:%!sed 's/ *; *$/ ;/'<cr>:w<cr>
+set noexpandtab
+
+highlight ColorColumn ctermbg=52| " set higlight to dark red
+call matchadd('ColorColumn', '\%81v', 100) " highlight 82 character of line
+set numberwidth=2
+
+filetype indent on " set indent depending on filetype
+set autoindent  " auto indent
+set tabstop=4   " The width of a TAB is set to 4.
+set softtabstop=4   " Sets the number of columns for a TAB
+set shiftwidth=4    " Indents will have a width of 4
+set expandtab   " Expand TABs to spaces
+
+exec "set listchars=tab:\uBB\uBB,trail:\uB7,nbsp:~"|    " trailing whitespaces
+set list    " enable trailing whitespaces detection
+
+let &path.="/usr/include,lib/catch/include,lib/spdlog/include,"
+set makeprg=./compile.sh
--- cmake/clang-cpp-tools.cmake
+++ cmake/clang-cpp-tools.cmake
@@ -1,12 +1,13 @@
-file(GLOB_RECURSE ALL_SOURCE_FILES src/*.[ch]pp)
+file(GLOB_RECURSE SOURCE_FILES src/*.[ch]pp)
 
 find_program(CLANG_FORMAT "clang-format")
 if(CLANG_FORMAT)
     add_custom_target(
         clang-format
-        COMMAND /usr/bin/clang-format -i ${ALL_SOURCE_FILES}
-        COMMAND /usr/bin/sed -i -f "semicolon-space" ${ALL_SOURCE_FILES}
+        COMMAND /usr/bin/clang-format -i ${SOURCE_FILES}
+        COMMAND /usr/bin/sed -i -f "${PROJECT_SOURCE_DIR}/cmake/semicolon-space" ${SOURCE_FILES}
         #TODO, put regex in command, not external file
+        #TODO, LINEBREAK
     )
 endif()
 
@@ -14,11 +15,11 @@ find_program(CLANG_TIDY "clang-tidy")
 if(CLANG_TIDY)
     add_custom_target(
         clang-tidy
-        COMMAND /usr/bin/clang-tidy -p=build/ ${ALL_SOURCE_FILES} 2> /dev/null
+        COMMAND /usr/bin/clang-tidy -p=build/ ${SOURCE_FILES} 2> /dev/null
     )
 
     add_custom_target(
         clang-tidy-fix
-        COMMAND /usr/bin/clang-tidy -p=build/ -fix ${ALL_SOURCE_FILES} 2> /dev/null
+        COMMAND /usr/bin/clang-tidy -p=build/ -fix ${SOURCE_FILES} 2> /dev/null
     )
 endif()
