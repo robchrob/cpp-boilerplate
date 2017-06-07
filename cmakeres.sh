1cd33e5dba89bba704a3d79e2b90c6505baff96c
Robert Chrobociński <robert.chrobocinski@gmail.com>
2017-06-07 20:55:23 +0200

    Failsafe from nuking everything

--- cmakeres.sh
+++ cmakeres.sh
@@ -3,9 +3,9 @@
 SCRIPT=$(realpath $0)
 SCRIPTPATH=$(dirname $SCRIPT)
 
-mkdir -p $SCRIPTPATH/build
-cd $SCRIPTPATH/build/
-rm -r *
+mkdir -p $SCRIPTPATH/build &&\
+cd $SCRIPTPATH/build/ &&\
+rm -rf * &&\
 cd ..
 ./compile.sh
 ./compile.sh #TODO, json database is not created at first time (?)
